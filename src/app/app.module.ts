f190aa6ecdce7a3da31cdc93e9a212248622f2a8
Kerunix <pierre_lafont@hotmail.fr>
2018-05-21 22:20:37 +0200

    Created home and list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WildersListComponent } from './wilders-list/wilders-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WildersListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/wilders-list/wilders-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wilders-list',
+  templateUrl: './wilders-list.component.html',
+  styleUrls: ['./wilders-list.component.css']
+})
+export class WildersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
